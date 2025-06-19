6bf482df5fac5d0133bdf467704a9436f8a78064
alkeshnikalje <alkeshnikalje@gmail.com>
2025-06-19 23:35:50 +0530

    capitalised cache struct fields to export it

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -4,10 +4,22 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+
+	"github.com/alkeshnikalje/go-pokedox/internal/pokecache"
 )
 
-func GetLocationAreas(url string) (*LocationAreaResponse, error) {
+func GetLocationAreas(url string,c *pokecache.Cache) (*LocationAreaResponse, error) {
 	var locationArea LocationAreaResponse
+	
+	entry, ok := c.Entries[url]
+	
+	if ok {
+		if err := json.Unmarshal(entry.val, &locationArea); err != nil {
+			return nil, err
+		}			
+		return &locationArea,nil
+	}
+
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -11,33 +11,33 @@ type cacheEntry struct {
 }
 
 type Cache struct {
-    entries  map[string]cacheEntry
-    mu       sync.Mutex
-	interval time.Duration
+    Entries  map[string]cacheEntry
+    Mu       sync.Mutex
+	Interval time.Duration
 }
 
 func NewCache(interval time.Duration) *Cache {
 	cache := Cache{
-		entries: make(map[string]cacheEntry),
-		interval: interval,
+		Entries: make(map[string]cacheEntry),
+		Interval: interval,
 	} 		
 	return &cache
 }
 
 func (c *Cache) Add(key string, val []byte) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
+	c.Mu.Lock()
+	defer c.Mu.Unlock()
 
-	c.entries[key] = cacheEntry{
+	c.Entries[key] = cacheEntry{
 		createdAt: time.Now(),
 		val: val,
 	}
 }
 
 func (c *Cache) Get(key string) ([]byte,bool) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	value,ok := c.entries[key]
+	c.Mu.Lock()
+	defer c.Mu.Unlock()
+	value,ok := c.Entries[key]
 	if !ok {
 		return []byte{},false
 	}
@@ -45,18 +45,18 @@ func (c *Cache) Get(key string) ([]byte,bool) {
 }
 
 func (c *Cache) readLoop() {
-	ticker := time.NewTicker(c.interval)
+	ticker := time.NewTicker(c.Interval)
 	defer ticker.Stop()
 
 	for {
 		<-ticker.C
-		c.mu.Lock()
-		for k,entry := range c.entries {
-			if time.Since(entry.createdAt) > c.interval {
-				delete(c.entries,k)
+		c.Mu.Lock()
+		for k,entry := range c.Entries {
+			if time.Since(entry.createdAt) > c.Interval {
+				delete(c.Entries,k)
 			}
 		}
-		c.mu.Unlock()
+		c.Mu.Unlock()
 	}
 } 
 
